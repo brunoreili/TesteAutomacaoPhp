f032327c8de24f3ec5b8fd250aac25fcf71aaace
brunoreili <brunoreili@gmail.com>
2017-08-29 23:52:50 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
          <?php 
             $url = file_get_contents('http://hometeste.dlinkddns.com');
             $result = preg_replace("/\<p\>(.*?)\<\/p\>/i", "\\1", $url);
+            $teste = 2;
             echo "$result";
+            echo "foi";
+            echo "$url";
+            echo "$teste";
         ?>
     </body>
 </html>
