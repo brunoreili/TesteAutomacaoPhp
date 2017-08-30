576d55c34eff965d5b6a6de3314c8a0e2b7b1ab4
brunoreili <brunoreili@gmail.com>
2017-08-30 01:45:22 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -63,9 +63,10 @@
         <script src="js/jquery-3.2.1.min.js"></script>
         <script src="js/materialize.min.js"></script>
         
+        <div id="storage" style="display:none;">
             <?php
             //Função retirada do forum "https://forum.imasters.com.br/topic/221364-erro-com-funcao-file_get_contents/" do usuário Daniel_Ribeiro
-            /*    function my_file_get_contents( $site_url ){
+                function my_file_get_contents( $site_url ){
                     $ch = curl_init();
                     $timeout = 10;
                     curl_setopt ($ch, CURLOPT_URL, $site_url);
@@ -75,15 +76,13 @@
                     curl_close($ch);
                     return $file_contents;
                     
-                }*/
+                }
             
                 //O problema estava no "file_get_contents" que pois, provavelmente o servidor desativou o acesso a arquivos atraves de url.
                 //Foi substituido então pelo "my_file_get_contents" utilizando a função acima.
                 $url = file_get_contents('https://testeautomacaohtml.herokuapp.com/');
                 $result = preg_replace("/\<p\>(.*?)\<\/p\>/i", "\\1", $url);
-            ?>
-        <div id="storage" style="display:none;">
-            <?php
+                
                 echo "$result";
             ?>
         </div>
