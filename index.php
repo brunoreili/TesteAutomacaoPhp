93d31628a5944bdd37b338b7842379114ac2a00a
brunoreili <brunoreili@gmail.com>
2017-08-29 23:16:22 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
         <script src="js/jquery-3.2.1.min.js"></script>
         <script src="js/materialize.min.js"></script>
          <?php 
-            $url = file_get_contents('http://hometeste.dlinkddns.com');
+            $url = file_get_contents('https://hometeste.dlinkddns.com/');
             $result = preg_replace("/\<p\>(.*?)\<\/p\>/i", "\\1", $url);
         ?>
         <div id="storage" style="display:none;">
