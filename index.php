1a6524ca8a186694b152d98545ebbc1ea7ff923d
brunoreili <brunoreili@gmail.com>
2017-08-30 00:04:14 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
     <head>              
     </head>
     <body>      
-         <?php 
-            $url = curl_get_contents('http://hometeste.dlinkddns.com');
+         <?php
+            function my_file_get_contents( $site_url ){	$ch = curl_init();	$timeout = 10;	curl_setopt ($ch, CURLOPT_URL, $site_url);	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);	curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);	$file_contents = curl_exec($ch);	curl_close($ch);	return $file_contents;}
+         
+         
+            $url = my_file_get_contents('http://hometeste.dlinkddns.com');
             $result = preg_replace("/\<p\>(.*?)\<\/p\>/i", "\\1", $url);
             $teste = 2;
             echo "$result";
